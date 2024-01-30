6db82576555d47eb79efe3ad6e9edf30cceb4596
Pedro <barbeitos.pedro@gmail.com>
2024-01-30 08:41:57 +0000

    basic tree added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,62 @@
+"use client";
+
 import Image from "next/image";
+import Tree from "react-d3-tree";
+import { useCenteredTree } from "@/lib/helpers";
+
+const orgChart = {
+  name: "CEO",
+  children: [
+    {
+      name: "Manager",
+      attributes: {
+        department: "Production",
+      },
+      children: [
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Assembly",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const containerStyles = {
+  width: "100vw",
+  height: "100vh",
+};
 
 export default function Home() {
+  const [dimensions, translate, containerRef] = useCenteredTree();
   return (
-    <main className="flex flex-col items-center justify-center "> main</main>
+    <main style={containerStyles} ref={containerRef}>
+      <Tree
+        data={orgChart}
+        dimensions={dimensions}
+        translate={translate}
+        draggable={true}
+        zoomable={true}
+        orientation="vertical"
+      />
+    </main>
   );
 }
