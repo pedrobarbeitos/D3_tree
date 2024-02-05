6b6612c177fe397a500d0686cdd457f2209a5754
Pedro <barbeitos.pedro@gmail.com>
2024-02-05 22:59:07 +0000

    tree container dimensioned

--- components/TreeContainer.tsx
+++ components/TreeContainer.tsx
@@ -9,18 +9,23 @@ const Tree = dynamic(() => import("react-d3-tree"), { ssr: false });
 export const TreeContainer = () => {
   const [dimensions, translate, containerRef] = useCenteredTree();
   return (
-    <div
-      className="w-full border-solid border-primary border-2 flex grow"
-      ref={containerRef}
-    >
-      <Tree
-        data={data}
-        translate={translate}
-        dimensions={dimensions}
-        draggable={true}
-        zoomable={true}
-        orientation="vertical"
-      />
+    <div className="w-full border-solid border-primary border-[1px] flex flex-col rounded-lg">
+      <div className="flex justify-center w-full pb-8 pt-4 gap-1 sm:gap-3">
+        oi
+      </div>
+      <div
+        className="w-full border-solid border-primary border-[1px] flex grow-[2] rounded-lg"
+        ref={containerRef}
+      >
+        <Tree
+          data={data}
+          translate={translate}
+          dimensions={dimensions}
+          draggable={true}
+          zoomable={true}
+          orientation="vertical"
+        />
+      </div>
     </div>
   );
 };
