ebcb118f0eee86f265a5ce16decd229c0ac79885
Pedro <barbeitos.pedro@gmail.com>
2024-02-08 13:32:29 +0000

    Render custom node element created

---
+++ components/SearchTreeInput.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+export function SearchTreeInput() {
+  return <Input type="search" placeholder="Search folders and files" />;
+}
--- components/TreeContainer.tsx
+++ components/TreeContainer.tsx
@@ -6,14 +6,44 @@ import { useCenteredTree } from "../lib/helpers";
 import { data } from "../lib/data";
 import { TreeOrientationButton } from "./TreeOrientationButton";
 import { NodePathButton } from "./NodePathButton";
+import { SearchTreeInput } from "./SearchTreeInput";
 const Tree = dynamic(() => import("react-d3-tree"), { ssr: false });
+
+interface NodeDatum {
+  name: string;
+  type?: string;
+}
+
+interface ForeignObjectProps {
+  width: number;
+  height: number;
+  x: number;
+}
+
+const renderForeignObjectNode = ({
+  nodeDatum,
+  foreignObjectProps,
+}: {
+  nodeDatum: NodeDatum;
+  foreignObjectProps: ForeignObjectProps;
+}) => (
+  <foreignObject {...foreignObjectProps}>
+    <div className="bg-popover w-[250px]  h-[75px] rounded-lg">
+      {nodeDatum.name}
+    </div>
+  </foreignObject>
+);
 
 export const TreeContainer = () => {
   const [dimensions, translate, containerRef] = useCenteredTree();
+  const nodeSize = { x: 300, y: 250 };
+  const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: -125 };
+  const separation = { siblings: 1, nonSiblings: 2 };
+
   return (
     <div className="w-full flex flex-col">
       <div className="flex justify-center w-full pb-2 gap-1 sm:gap-3">
-        <TreeOrientationButton /> <NodePathButton />
+        <TreeOrientationButton /> <NodePathButton /> <SearchTreeInput />
       </div>
       <div
         className="w-full border-solid border-primary border-[1px] flex grow-[2] rounded-lg"
@@ -23,9 +53,14 @@ export const TreeContainer = () => {
           data={data}
           translate={translate}
           dimensions={dimensions}
+          separation={separation}
           draggable={true}
           zoomable={true}
-          orientation="horizontal"
+          orientation="vertical"
+          nodeSize={nodeSize}
+          renderCustomNodeElement={(rd3tProps) =>
+            renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
+          }
         />
       </div>
     </div>
